efa1d5d72b87c7ade0112ac249c72768559ff53a
Di Zheng <dizhengcindy@gmail.com>
2020-02-21 18:08:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,9 +49,11 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   result = []
+  hash = {}
   index = 0 
   while index < movies_collection.length do
-  result << movie_with_director_name(name,movies_collection[index])
+  hash =  movie_with_director_name(name,movies_collection[index])
+  result[hash[:director_name]] = hash[:title]
   index += 1 
  end
   result
